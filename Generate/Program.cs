129ddcd1dcb769aec9c1090ba1fb37978e691ee7
ofmxtheuuz <mxtheuzchika@gmail.com>
2023-01-09 01:59:49 -0300

    initial commit

---
+++ Generate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Channels;
+
+Random rand = new Random();
+
+string Generate()
+{
+    string max = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJQLMNOPQRSTUVWXYZ1234567890";
+    var str = ""; 
+
+    for (int i = 0; i < 42; i++)
+    {
+        str += max[(int)Math.Floor((decimal)rand.Next(0, max.Length))];
+    }
+    
+    return str + "+";
+}
+
+while(true)
+{
+    var x = rand.Next(1, 20);
+    Console.ResetColor();
+    if (x != 5)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"[-] {Generate()}: Invalid code");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"[-] {Generate()}: Success code");
+    }
+    Thread.Sleep(200);
+}
